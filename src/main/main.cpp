35df95952d020badaf96eddd2bb638570667a09d
ivanfedotov <ivan78990@gmail.com>
2022-09-20 14:39:59 +0300

    validateConfigFile

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -6,28 +6,25 @@
 #include "Server.hpp"
 
 
-bool isIncorrectConfigFile(const std::string& configFileName) {
-    if (Utils::isFileExists(configFileName)) {
-        if (Utils::isAccessRights(configFileName))
-            return false;
+void validateConfigFile(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        std::cerr << "Did not provide a configuration file" << std::endl;
+        exit(1);
     }
-	return true;
+    const std::string configFileName = argv[1];
+
+    if (!Utils::isFileExists(configFileName) || !Utils::isAccessRights(configFileName)) {
+        std::cerr << "Incorrect config file" << std::endl;
+        exit(1);
+    }
 }
 
 int main(int argc, char *argv[]) {
 
-//  mplement input config file
-	if (argc != 2) {
-		std::cerr << "Did not provide a configuration file" << std::endl;
-		exit(1);
-	}
-	if (isIncorrectConfigFile(argv[1])) {
-		std::cerr << "Incorrect config file" << std::endl;
-		exit(1);
-	}
+    validateConfigFile(argc, argv);
 
     string configFileName = argv[1];
-
 
     ConfigParser* configParser = new ConfigParser();
     ConfigStorage* configStorage = new ConfigStorage();
